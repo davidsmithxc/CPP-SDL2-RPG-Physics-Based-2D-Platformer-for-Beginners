fee24326a5164172baaedaff3aec640505c6509c
David Smith <davidsmithxc@gmail.com>
2020-09-06 14:15:37 -0700

    tutorial pt. 4 - loading textures - drawing first texture to window - adding graphics assets

--- src/main.cpp
+++ src/main.cpp
@@ -14,10 +14,14 @@ int main(int argc, char* args[])
 
     RenderWindow window("Game v1.0", 1280, 720);
 
+    // load textures
+    SDL_Texture* grassTexture = window.loadTexture("res/gfx/ground_grass_1.png");
+    
     // set up game loop
     bool gameRunning = true;
     SDL_Event event;
 
+    // start game loop
     while(gameRunning)
     {
         while(SDL_PollEvent(&event))
@@ -25,8 +29,13 @@ int main(int argc, char* args[])
             if(event.type == SDL_QUIT)
                 gameRunning = false;
         }
+
+        window.clear();
+        window.render(grassTexture);
+        window.display();
     }
 
+    // exit and clean up
     window.cleanUp();
     SDL_Quit();
     
--- src/renderwindow.cpp
+++ src/renderwindow.cpp
@@ -19,3 +19,30 @@ void RenderWindow::cleanUp()
 {
     SDL_DestroyWindow(window);
 }
+
+SDL_Texture* RenderWindow::loadTexture(const char* p_filePath)
+{
+    //TODO: update to use smart pointers, move sematics?
+    SDL_Texture* texture = NULL;
+    texture = IMG_LoadTexture(renderer, p_filePath);
+
+    if (texture == NULL)
+        std::cout << "Failed to laod texture. Error: " << SDL_GetError() << std::endl;
+
+    return texture;
+}
+
+void RenderWindow::clear()
+{
+    SDL_RenderClear(renderer);
+}
+
+void RenderWindow::render(SDL_Texture* p_tex)
+{
+    SDL_RenderCopy(renderer, p_tex, NULL, NULL);
+}
+
+void RenderWindow::display()
+{
+    SDL_RenderPresent(renderer);
+}
